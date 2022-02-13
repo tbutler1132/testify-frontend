48cad141a4fff50568e96f3a179b785473693340
Timothy Butler <tbutler1132@gmail.com>
2022-02-13 08:34:46 -0500

    First styling

--- src/containers/VoteOnTestContainer.jsx
+++ src/containers/VoteOnTestContainer.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import { useGetRandomTestQuery, useUpdateMediaMutation } from '../redux/services/testify';
 
 function VoteOnTestContainer(props) {
     const { data, error, isFetching, refetch } = useGetRandomTestQuery('bulbasaur')
     const [voteOnTest, {isLoading}] = useUpdateMediaMutation()
+    const [queuedVote, setVote] = useState(null)
 
     const voteHandler = (e, i) => {
-        const updatedMedia = {...data.media[i], votes: data.media[i].votes + 1}
+        setVote(data.media[i])
+    }
+
+    const submitHandler = () => {
+        const updatedMedia = {...queuedVote, votes: queuedVote.votes + 1}
         console.log(updatedMedia)
-        voteOnTest({userId: data.userId, testId: data.testId, mediaId: data.media[i]._id, media: updatedMedia})
+        voteOnTest({userId: data.userId, testId: data.testId, mediaId: queuedVote._id, media: updatedMedia})
+        setVote(null)
+        refetch()
     }
 
     return (
@@ -16,11 +24,12 @@ function VoteOnTestContainer(props) {
             {isFetching ? <div>Loading...</div> 
             :
             <>
+            <h3>You've currently selected {queuedVote?.title}</h3>
             <label >{data.media[0].title}</label>
             <button onClick={(e) => voteHandler(e, 0)}>Vote</button>
             <label>{data.media[1].title}</label>
             <button onClick={(e) => voteHandler(e, 1)}>Vote</button>
-            <button onClick={refetch}>Submit</button>
+            <button onClick={submitHandler}>Submit</button>
             </>
             }
         </div>
